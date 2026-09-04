b0cebb09cbac797a7a960d4526946fbc07f1301b
Tony Fu <tony.tc.fu@gmail.com>
2026-09-04 11:39:56 +0800

    feat: sync data and dashboard updates on 2026-09-04 11:39:56

--- scripts/validate_research_brief_metrics.py
+++ scripts/validate_research_brief_metrics.py
@@ -8,8 +8,8 @@ from validate_research_brief_sync import DEFAULT_DASHBOARD, extract_research_bri
 
 
 EXPECTED_METRICS = {
-    "ai_weight_percent": "24.62%",
-    "risk_contribution_percent": "32.24%",
+    "ai_weight_percent": "24.61%",
+    "risk_contribution_percent": "32.23%",
     "risk_weight_gap_percent": "+7.62%",
     "trade_count": "0",
 }
